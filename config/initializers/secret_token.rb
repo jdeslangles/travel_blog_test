be21aa42baeed0d727286f00204d124b62d607f0
jdeslangles <julien.deslangles@gmail.com>
2013-08-26 13:09:08 +0100

    Initial commit, trying geocoder and gmpap4rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelBlogTest::Application.config.secret_token = '572afef09d171b82fbf0a8962adbad97a51ed1a3f70fc878f4db4d7e3a8d1f2203f9560b33352cd1d7066b1db4bb745346be68c1b8591acc11680ba700337281'
---
+++ db/migrate/20130825210102_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :firstname
+      t.string :lastname
+      t.date :birthdate
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
